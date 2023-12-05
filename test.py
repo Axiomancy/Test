ec18a4f74cbf6b6398052552d8df688a5be7d997
Axiomancy <dzreynolds91@googlemail.com>
2023-12-05 20:53:50 +0000

    this is my third git bash push for test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-print("Hello world")
-user_input = input("please enter your password")
-while user_input != "123":
-	user_input = input("invalid, try again!")
+#print("Hello world")
+#user_input = input("please enter your password")
+#while user_input != "123":
+#	user_input = input("invalid, try again!")
+
+for i in range(1,100):
+	if i >= 20:
+		continue
+	print(i)
+
 
